a2c13c43b0a963e1923fa524f852fde8d42e789e
geoitl20 <96070275+geoitl20@users.noreply.github.com>
2023-05-21 21:20:41 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,137 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "enemyDog":return img`
+. . 4 4 4 . . . . 4 4 4 . . . . 
+. 4 5 5 5 e . . e 5 5 5 4 . . . 
+4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+. e e 5 5 5 5 5 5 5 5 e e . . . 
+. . e 5 f 5 5 5 5 f 5 e . . . . 
+. . f 5 5 5 4 4 5 5 5 f . . f f 
+. . f 4 5 5 f f 5 5 6 f . f 5 f 
+. . . f 6 6 6 6 6 6 4 4 f 5 5 f 
+. . . f 4 5 5 5 5 5 5 4 4 5 f . 
+. . . f 5 5 5 5 5 4 5 5 f f . . 
+. . . f 5 f f f 5 f f 5 f . . . 
+. . . f f . . f f . . f f . . . 
+`;
+            case "image2":
+            case "enemyMonkey":return img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . f f 
+c c c c c d d d e e f c . f e f 
+. f d d d d d e e f f . . f e f 
+. . f f f f f e e e e f . f e f 
+. . . . f e e e e e e e f f e f 
+. . . f e f f e f e e e e f f . 
+. . . f e f f e f e e e e f . . 
+. . . f d b f d b f f e f . . . 
+. . . f d d c d d b b d f . . . 
+. . . . f f f f f f f f f . . . 
+`;
+            case "image3":
+            case "myHero":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image4":
+            case "myHeroSword":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+1 d f e f b f 4 4 f b f e f f . 
+d d d d 4 1 f d d f 1 4 e e f . 
+. d d d e d d d d d d e e f . . 
+. d 1 d d e 4 4 4 4 e e f . . . 
+. . d d 1 d d 2 2 2 2 f 4 e . . 
+. . 1 d d d 2 2 2 2 2 f d 4 . . 
+. . 4 4 d d 4 5 5 4 4 f 4 4 . . 
+. . . d d f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image5":
+            case "Fireball":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . 4 . . . . 
+. . . . 5 . . . . . . . . . . . 
+. . . . . . 4 4 5 . . . . . . . 
+. . . . . 4 4 4 4 4 . 5 . . . . 
+. . . . 4 4 5 4 5 4 . . . . . . 
+. . . . . 4 4 4 4 4 . . . . . . 
+. . . . . . 5 5 4 . . . . . . . 
+. . . 5 . . . . . . . . . . . . 
+. . . . . . . . . . . . 5 . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image6":
+            case "heroWalkFront1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 9 9 9 9 . . . . . . 
+. . . . . 9 9 9 9 9 9 . . . . . 
+. . . . 9 9 9 9 9 9 9 9 . . . . 
+. . . 9 9 9 f 3 3 f 9 9 9 . . . 
+. . . 9 3 1 f 3 3 f 1 3 9 . . . 
+. . . 9 3 3 3 3 3 3 3 3 9 . . . 
+. . . 9 9 3 3 3 3 3 3 9 9 . . . 
+. . . 9 9 b 3 3 3 3 b 9 9 . . . 
+. . . 9 b 7 7 7 7 7 7 b 9 . . . 
+. . . b b e 7 7 7 7 e b b . . . 
+. . . b 3 b e 7 7 e b 3 b . . . 
+. . . . . b b e e b b . . . . . 
+. . . . . 7 7 . . 7 7 . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,15 +1,40 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    lastDirection = 1
+})
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    if (lastDirection == 0) {
+        projectile = sprites.createProjectileFromSprite(assets.image`Fireball`, playerSprite, 100, 0)
+    }
+    if (lastDirection == 1) {
+        projectile = sprites.createProjectileFromSprite(assets.image`Fireball`, playerSprite, 0, -100)
+    }
+    if (lastDirection == 2) {
+        projectile = sprites.createProjectileFromSprite(assets.image`Fireball`, playerSprite, -100, 0)
+    }
+    if (lastDirection == 3) {
+        projectile = sprites.createProjectileFromSprite(assets.image`Fireball`, playerSprite, 0, 100)
+    }
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    playerSprite.setImage(assets.image`myHeroSword`)
+    swordOut = 1
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    lastDirection = 2
 })
 function loadLevel () {
+    for (let value of enemyList) {
+        sprites.destroy(value)
+    }
+    enemyList = []
     if (Level == 5050) {
         tiles.setCurrentTilemap(tilemap`level5050`)
     }
     if (Level == 5051) {
         tiles.setCurrentTilemap(tilemap`level5051`)
+        enemyList = [sprites.create(assets.image`enemyDog`, SpriteKind.Enemy), sprites.create(assets.image`enemyMonkey`, SpriteKind.Enemy)]
+        enemyX = [30, 100]
+        enemyY = [30, 60]
     }
     if (Level == 5049) {
         tiles.setCurrentTilemap(tilemap`level5049`)
@@ -29,32 +54,50 @@ function loadLevel () {
     if (playerSprite.tilemapLocation().row == 0) {
         tiles.placeOnTile(playerSprite, tiles.getTileLocation(playerSprite.tilemapLocation().column, 14))
     }
+    for (let index = 0; index <= enemyList.length - 1; index++) {
+        enemyList[index].setPosition(enemyX[index], enemyY[index])
+        enemyList[index].setVelocity(randint(-20, 20), randint(-20, 20))
+        enemyList[index].setStayInScreen(false)
+        enemyList[index].setBounceOnWall(true)
+    }
 }
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    lastDirection = 0
+})
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    playerSprite.setImage(assets.image`myHero`)
+    swordOut = 0
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    lastDirection = 3
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.fire, 500)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    if (swordOut == 0) {
+    	
+    } else {
+        sprites.destroy(otherSprite, effects.spray, 500)
+    }
+})
+let enemyY: number[] = []
+let enemyX: number[] = []
+let projectile: Sprite = null
+let swordOut = 0
+let enemyList: Sprite[] = []
 let playerSprite: Sprite = null
 let Level = 0
+let lastDirection = 0
+lastDirection = 0
 Level = 5050
-playerSprite = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
+playerSprite = sprites.create(assets.image`myHero`, SpriteKind.Player)
 controller.moveSprite(playerSprite, 100, 100)
+enemyList = []
 loadLevel()
 tiles.placeOnTile(playerSprite, tiles.getTileLocation(5, 3))
 scene.cameraFollowSprite(playerSprite)
+swordOut = 0
 forever(function () {
     if (playerSprite.tilemapLocation().column < 1) {
         Level += -1
